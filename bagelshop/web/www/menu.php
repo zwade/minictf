1a135bf33b0875e749056c7765e5f859d8f17345
zaratec <carolinamz1@gmail.com>
2019-01-27 20:54:58 +0000

    Add Bagel Shop blind sqli challenge

---
+++ bagelshop/web/www/menu.php
@@ -0,0 +1,48 @@
+<html>
+  <?php include("head.html"); ?>
+
+  <body>
+    <?php include("navbar.html") ?>
+
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light" style="background: url('bagel_30.jpg') no-repeat -500px -100px; margin:0px !important; height:300px;">
+      <div class="col-md-8 p-lg-5 mx-auto my-5">	  
+        <h1 style="color:white;">Bagel Menu</h1>
+	<p class="lead font-weight-normal" style="color:white;">Check our selection of available bagels.</p>
+      </div>
+    </div>
+
+    <div class="row justify-content-center" style="padding-top:10px;">
+    	 <div class="col-2">
+	   <h2>Yummy</h2>
+	   <ul>
+	     <li>Everything</li>
+	     <li>Cheesy</li>
+	     <li>Garlic</li>
+	     <li>Onion</li>
+	     <li>PoppySeed</li>
+	     <li>SesameSeed</li>
+	   </ul>
+	 </div>
+	 <div class="col-2">
+	   <h2>Sweet!</h2>
+	   <ul>
+	     <li>CinnamonSugar</li>
+	     <li>FrenchToast</li>
+	     <li>ChocolateChip</li>
+	     <li>CinnamonRaisin</li>
+	   </ul>
+	 </div>
+	 <div class="col-2">
+	   <h2>Healthy</h2>
+	   <ul>
+	     <li>Blueberry</li>
+	     <li>Cranberry</li>
+	     <li>Spinach</li>
+	     <li>Protein</li>
+	     <li>Flag</li>
+	   </ul>
+	 </div>
+    </div>
+ 
+  </body>
+</html>
